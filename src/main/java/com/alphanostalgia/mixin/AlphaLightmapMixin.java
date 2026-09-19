6ad7fb79d916e4a4eac13e0bf6fef2ecb83a02f3
marcossporleder <marcos.sporleder@gmail.com>
2026-09-19 10:18:23 +0200

    Create AlphaLightmapMixin.java

---
+++ src/main/java/com/alphanostalgia/mixin/AlphaLightmapMixin.java
@@ -0,0 +1,16 @@
+package com.alphanostalgia.mixin;
+
+import net.minecraft.client.renderer.LightTexture;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LightTexture.class)
+public class AlphaLightmapMixin {
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private static void forceHarshAlphaLighting(float flash, int lightLevel, CallbackInfoReturnable<Float> cir) {
+        float rawLight = (float)lightLevel / 15.0f;
+        cir.setReturnValue(rawLight);
+    }
+}
