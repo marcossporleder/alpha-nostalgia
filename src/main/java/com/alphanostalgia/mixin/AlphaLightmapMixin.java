0cb1f43290a0029ceac680b747f6e9afb770a0ac
marcossporleder <marcos.sporleder@gmail.com>
2026-09-19 11:11:02 +0200

    Update AlphaLightmapMixin.java

--- src/main/java/com/alphanostalgia/mixin/AlphaLightmapMixin.java
+++ src/main/java/com/alphanostalgia/mixin/AlphaLightmapMixin.java
@@ -8,10 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LightTexture.class)
 public class AlphaLightmapMixin {
-    
     @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
     private static void forceHarshAlphaLighting(float flash, int lightLevel, CallbackInfoReturnable<Float> cir) {
-        // Safe math clamp that linearizes 26.3 illumination step maps cleanly
         float rawLight = (float)(lightLevel & 15) / 15.0f;
         cir.setReturnValue(rawLight);
     }
