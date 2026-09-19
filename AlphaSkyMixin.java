4f9568067aa0ecda14d296f03d7ee38e928a0c6c
marcossporleder <marcos.sporleder@gmail.com>
2026-09-19 10:08:06 +0200

    Create AlphaSkyMixin.java

---
+++ AlphaSkyMixin.java
@@ -0,0 +1,17 @@
+package com.alphanostalgia.mixin;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientLevel.Properties.class)
+public class AlphaSkyMixin {
+    @Inject(method = "getSkyColor", at = @At("HEAD"), cancellable = true)
+    private void overrideSkyColor(CallbackInfoReturnable<Vec3> cir) {
+        // Hex conversion to match the exact saturated day-sky background vector
+        cir.setReturnValue(new Vec3(0.533, 0.702, 1.0));
+    }
+}
